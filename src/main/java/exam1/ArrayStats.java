834c00694fd3a5af8d07c06f3e679b28308bcb66
Javuchon <javidragon.s98@gmail.com>
2025-11-26 17:02:16 +0100

    EX1: ArrayStats suma
    # Conflicts:
    #	src/main/java/exam1/ArrayStats.java

--- src/main/java/exam1/ArrayStats.java
+++ src/main/java/exam1/ArrayStats.java
@@ -15,17 +15,22 @@ public class ArrayStats {
         for(int i = 0; i < n; i++){
             array[i] = in.nextInt();
         }
+
         int minim = Integer.MAX_VALUE;
         int maxim = Integer.MIN_VALUE;
+        int suma = 0;
         for(int i = 0; i < array.length; i++){
             if(minim > array[i])
                 minim = array[i];
             if(maxim < array[i])
                 maxim = array[i];
+
+            suma += array[i];
         }
 
         System.out.printf("L'element més xicotet és: %d\n", minim);
         System.out.printf("L'element més gran és: %d\n", maxim);
+        System.out.printf("La suma total és: %d\n", suma);
 
     }
 }
